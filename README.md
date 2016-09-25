fa469ec0631ddbe3634e4fd0cc0649adf53a5dac
neel <neelwithsmile@gmail.com>
2016-09-25 13:18:14 -0700

    Add graphs

--- README.md
+++ README.md
@@ -1,20 +1,2 @@
-
-
-# Twitter-SQL
-
-
-
-## Usage
-
-
-
-## Developing
-
-
-
-### Tools
-
-Created with [Nodeclipse](https://github.com/Nodeclipse/nodeclipse-1)
- ([Eclipse Marketplace](http://marketplace.eclipse.org/content/nodeclipse), [site](http://www.nodeclipse.org))   
-
-Nodeclipse is free open-source project that grows with your contributions.
+# SFDLP_WC-Eagle
+SF Day Labor Program and Woman’s Collective - Team Eagle
--- routes/mysql.js
+++ routes/mysql.js
@@ -2,28 +2,15 @@ var ejs= require('ejs');
 var mysql = require('mysql');
 var connectionPool = require('./connectionPool');
 
-function fetchData(callback,sqlQuery){
-	var connection=connectionPool.getConnectionFromConnectionPool();	
+function fetchData(sqlQuery,callback){
+	var connection=connectionPool.getConnectionFromConnectionPool();
 	connection.query(sqlQuery, function(err, rows, fields) {
 		if(err){
 			console.log("ERROR: " + err.message);
 		}
-		callback(err, rows);		
+		callback(err, rows);
 	});
 	connectionPool.releaseConnection(connection);
-}	
-
-
-function fetchData1(sqlQuery,callback){
-	var connection=connectionPool.getConnectionFromConnectionPool();	
-	connection.query(sqlQuery, function(err, rows, fields) {
-		if(err){
-			console.log("ERROR: " + err.message);
-		}
-		callback(err, rows);		
-	});
-	connectionPool.releaseConnection(connection);
-}	
+}
 
 exports.fetchData=fetchData;
-exports.fetchData1=fetchData1;
