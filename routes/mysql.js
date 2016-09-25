066accf790ae1cc3b011125b4d6edb3102743edc
neel <neelwithsmile@gmail.com>
2016-09-25 13:53:26 -0700

    Bug fix

--- routes/mysql.js
+++ routes/mysql.js
@@ -2,7 +2,18 @@ var ejs= require('ejs');
 var mysql = require('mysql');
 var connectionPool = require('./connectionPool');
 
-function fetchData(sqlQuery,callback){
+function fetchData(callback,sqlQuery){
+	var connection=connectionPool.getConnectionFromConnectionPool();
+	connection.query(sqlQuery, function(err, rows, fields) {
+		if(err){
+			console.log("ERROR: " + err.message);
+		}
+		callback(err, rows);
+	});
+	connectionPool.releaseConnection(connection);
+}
+
+function fetchData1(sqlQuery,callback){
 	var connection=connectionPool.getConnectionFromConnectionPool();
 	connection.query(sqlQuery, function(err, rows, fields) {
 		if(err){
@@ -14,3 +25,4 @@ function fetchData(sqlQuery,callback){
 }
 
 exports.fetchData=fetchData;
+exports.fetchData1=fetchData1;
